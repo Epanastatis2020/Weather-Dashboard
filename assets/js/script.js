b4ef7e309f85a472f87e9df404b636df7b21d6e8
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2020-09-20 12:39:20 +1000

    added bones of document initialiser in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 //Declare global variables here
+var APIkey = '&appid=4d90ce9f0a7bd1882a4f86d3d0c1088b';
+var weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?';
+var uviAPI = 'https://api.openweathermap.org/data/2.5/uvi?lat=';
+var forecastAPI = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+var geoAPI = navigator.geolocation;
+var units = '&units=metric';
+var getWeatherIcon = 'http://openweathermap.org/img/wn/';
 
 //Document ready initialiser
+$(document).ready(function() {
+    init();
+
+    function init() {
+
+    }
+});
 
 //Event listeners
 
