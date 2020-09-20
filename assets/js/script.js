c24c614dc3e4b41661471a474dc5215ac67cdbcc
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2020-09-20 16:22:07 +1000

    started retrieveForcast function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,53 @@
 //Declare global variables here
-var APIkey = '&appid=4d90ce9f0a7bd1882a4f86d3d0c1088b';
-var weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?';
-var uviAPI = 'https://api.openweathermap.org/data/2.5/uvi?lat=';
-var forecastAPI = 'https://api.openweathermap.org/data/2.5/forecast?q=';
-var geoAPI = navigator.geolocation;
-var units = '&units=metric';
-var getWeatherIcon = 'http://openweathermap.org/img/wn/';
+const APIkey = '&appid=4d90ce9f0a7bd1882a4f86d3d0c1088b';
+const weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?';
+const uviAPI = 'https://api.openweathermap.org/data/2.5/uvi?lat=';
+const forecastAPI = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+const geoAPI = navigator.geolocation;
+const units = '&units=metric';
+const getWeatherIcon = 'http://openweathermap.org/img/wn/';
+let city;
 
 //Document ready initialiser
 $(document).ready(function() {
-    init();
+    //init();
 
-    function init() {
+    //function init() {
 
-    }
+    //}
+
+    //Event listeners
+    $('#searchBtn').on('click', search());
 });
 
-//Event listeners
+
 
 //Search function
+function search() {
+    city = $('#searchInput')
+        .val()
+        .trim();
+    
+    if (city === "") {
+        return;
+    }
+    $('#searchInput').val("");
+    retrieveForecast(city);
+};
 
 //Retrieve forecast function
+
+function retrieveForecast(search) {
+    let queryURL = weatherAPI + 'q=' + search + units + APIkey;
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET',
+
+    }).then(function(response) {
+        console.log(response);
+    })
+}
 
 //Function to detect current user geolocation
 
