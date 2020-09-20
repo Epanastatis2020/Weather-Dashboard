02573b616dbd7b9c4ce8b628d40b59f2f6e0f4a0
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2020-09-20 12:37:00 +1000

    created comment structure for Javascript

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+//Declare global variables here
+
+//Document ready initialiser
+
+//Event listeners
+
+//Search function
+
+//Retrieve forecast function
+
+//Function to detect current user geolocation
+
+//Function to create history array
+
+//Function to save search history
+
+//Function to show search history
+
+//Function to clear search history
+
+//Function to retrieve forecast from history
